dee19c44722f77f1e36169c250ceae8c78804cb8
exuteboll <exuteboll4@gmail.com>
2024-12-01 19:34:41 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Task 1  Найти минимальную величину из двух целых переменных a, b,
+    /// используя вспомогательные методы (ввода аргументов, нахождения минимальной величины, вывода результата).
+    /// </summary>
+    class Program
+    {
+        static int Min(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+        static int Input(string msg)
+        {
+            Console.Write(msg);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            int x = Input("x= ");
+            int y = Input("y= ");
+            int z = Input("z= ");
+            int min = Min(Min(x, y), z);
+            Console.WriteLine($"Минимальное значение = {min}");
+            Console.ReadKey();
+        }
+    }
+}
